3073e6049c9cd93d40fff77c067f7b016027b093
PashaLopatuk <60324515+PashaLopatuk@users.noreply.github.com>
2023-01-29 15:59:05 +0200

    Add files via upload

--- starbucks/script/menu.js
+++ starbucks/script/menu.js
@@ -14,8 +14,8 @@ document.querySelector('.header__case .menu .button').addEventListener('click', 
     if (!is) {
         button.style.backgroundColor = '#047044';
         button.style.color = '#fff';
-        document.querySelector('.header__container').style.height = px(200);
-        document.querySelector('.header__container').style.marginTop = px(60);
+        document.querySelector('.header__container').style.height = '15vh';
+        document.querySelector('.header__container').style.marginTop = '2vh';
         header.style.borderRadius = px(35)
         header.style.alignItems = 'start';
         headerCase.style.marginTop = px(15);
@@ -23,10 +23,11 @@ document.querySelector('.header__case .menu .button').addEventListener('click', 
         
         menuList.style.display = 'flex';
         menuList.style.position = 'absolute';
-        menuList.style.left = px(60);
+        menuList.style.justifyContent = 'center';
+        menuList.style.left = px(0);
         menuList.style.width = '100%';
-        menuList.style.top = px(200);
-        menuList.style.fontSize = px(20)
+        menuList.style.top = '14.5vh';
+        menuList.style.fontSize = '4vw';
         "position: absolute;\njustify-self: center;\nleft: 60px;\nwidth: 100%;\ntop: 160px;"   
         
         is = !is;
@@ -47,7 +48,7 @@ document.querySelector('.header__case .menu .button').addEventListener('click', 
         menuList.style.position = '';
         menuList.style.left = 0;
         menuList.style.top = 0;
-        document.querySelector('.menu .list ').style.fontSize = '16px'
+        // document.querySelector('.menu .list ').style.fontSize = '12vw'
         document.querySelector('.header__container').style.boxShadow = '0px 0px 0px 0px rgba(0,0,0, 0);'
         is = !is;
     }
